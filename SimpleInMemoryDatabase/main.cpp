4ea24ed794de3a8a2990cbfc8522b9540e3a04b0
Xiaojun <wxj995@gmail.com>
2017-10-09 04:19:55 -0400

    Signed-off-by: Xiaojun <wxj995@gmail.com>

--- SimpleInMemoryDatabase/main.cpp
+++ SimpleInMemoryDatabase/main.cpp
@@ -22,9 +22,9 @@
 #include <unordered_map>
 using namespace std;
 
-
+//The assignment description is here:
 //https://www.thumbtack.com/challenges/simple-database
-//In the Simple Database problem, you'll implement an in-memory database similar to Redis. 
+//In the simple database problem, you'll implement an in-memory database similar to Redis. 
 //For simplicity's sake, instead of dealing with multiple clients and communicating over the network, 
 //your program will receive commands via standard input(stdin), 
 //and should write appropriate responses to standard output(stdout).
